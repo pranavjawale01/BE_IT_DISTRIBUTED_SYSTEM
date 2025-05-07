1b60342d7c9e2ea4eda1c8c5ff8ab9765bbfe6db
pranavjawale01 <jawalepranav01@gmail.com>
2025-05-07 17:25:02 +0530

    Readme

--- Assignment-1_RMI/ServerInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerInterface extends Remote {
-    int add(int a, int b) throws RemoteException;
-    int sub(int a, int b) throws RemoteException;
-    int mul(int a, int b) throws RemoteException;
-    int div(int a, int b) throws RemoteException;
-}
